198b37dc8aca96dc2c8f112130fe84d548317572
Rae Duqs <raeaddison.duque@cit.edu>
2024-11-02 13:17:00 +0800

    Added foreign key in Tasks for User Account

--- BubbleSpaceProject/Tasks/models.py
+++ BubbleSpaceProject/Tasks/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+# models.py
 
-# Create your models here.
+from django.conf import settings
+
 class Task(models.Model):
     title = models.CharField(max_length=255)
-
     CATEGORY_CHOICES = [
         ('Category 1', 'Category 1'),
         ('Category 2', 'Category 2'),
@@ -11,19 +12,24 @@ class Task(models.Model):
         ('Category 4', 'Category 4'),
         ('Category 5', 'Category 5'),
     ]
-
     STATUS_CHOICES = [
         ('green', 'Green'),
         ('yellow', 'Yellow'),
         ('red', 'Red'),
     ]
-
     task_id = models.AutoField(primary_key=True)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     due_date = models.DateField()
     description = models.TextField()
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
     assigned_to = models.CharField(max_length=100)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE,
+        related_name='tasks',
+        
+    )
 
-    def __str__ (self):
+    def __str__(self):
         return self.title
+
--- BubbleSpaceProject/Tasks/views.py
+++ BubbleSpaceProject/Tasks/views.py
@@ -3,27 +3,37 @@ from django.urls import reverse
 from .models import Task
 from .forms import TaskForm
 
-# Create your views here.
+## views.py
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+
+@login_required
 def task_list(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(created_by=request.user)
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
+@login_required
 def task_detail(request, pk):
-    task = get_object_or_404(Task, pk=pk)
+    task = get_object_or_404(Task, pk=pk, created_by=request.user)
     return render(request, 'tasks/task_detail.html', {'task': task})
 
+@login_required
 def task_create(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()
+            task = form.save(commit=False)
+            task.created_by = request.user
+            task.save()
             return redirect('task_list')
     else:
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form, 'title': 'Create New Task'})
 
+@login_required
 def task_update(request, pk):
-    task = get_object_or_404(Task, pk=pk)
+    task = get_object_or_404(Task, pk=pk, created_by=request.user)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
@@ -33,8 +43,9 @@ def task_update(request, pk):
         form = TaskForm(instance=task)
     return render(request, 'tasks/task_form.html', {'form': form, 'title': 'Update Task'})
 
+@login_required
 def task_delete(request, pk):
-    task = get_object_or_404(Task, pk=pk)
+    task = get_object_or_404(Task, pk=pk, created_by=request.user)
     if request.method == 'POST':
         task.delete()
         return redirect('task_list')
