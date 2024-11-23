6675a55e5af65ed8cd2935300d4adbd55a0065e0
Kazuchii <153843649+Kazuchii@users.noreply.github.com>
2024-11-23 16:29:53 +0800

    Removed Status Field in Project Form

--- BubbleSpaceProject/Projects/forms.py
+++ BubbleSpaceProject/Projects/forms.py
@@ -4,9 +4,8 @@ from .models import Project
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['Title', 'Description', 'Status']
+        fields = ['Title', 'Description']
         widgets = {
-            'Status': forms.Select(attrs={'class': 'form-control'}),
             'Title': forms.TextInput(attrs={'class': 'form-control'}),
             'Description': forms.Textarea(attrs={'class': 'form-control', 'maxlength': 1000}),
         }
