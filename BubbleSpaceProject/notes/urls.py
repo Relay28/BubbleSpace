20a4c6e211096613098d9f1427632a344c7ac83c
Jeannie Margarett Verar <jeanniemargarett.verar@cit.edu>
2024-11-29 17:59:16 +0800

    Fixed margin and UI & added edit and delete icons

--- BubbleSpaceProject/notes/urls.py
+++ BubbleSpaceProject/notes/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.notes_list, name='notes_list'),
     path('add/', views.add_note, name='add_note'),
     path('note-detail/<int:pk>/', views.note_detail_api, name='note_detail_api'),
-    # path('note-detail/<int:pk>/edit/', views.edit_note, name='edit_note'),
-    # path('note-detail/<int:pk>/delete/', views.delete_note, name='delete_note'),
+    path('<int:pk>/edit/', views.edit_note, name='edit_note'),  # Ensure this URL is added
+    path('<int:pk>/delete/', views.delete_note, name='delete_note'),
 ]
